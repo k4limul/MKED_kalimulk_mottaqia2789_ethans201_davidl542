8a2244d3854257c81ae8df82b6516097849f4327
dlee08 <davidlee03112008@gmail.com>
2025-12-05 11:31:59 -0500

    updated requirements.txt, .gitignore' '

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,23 @@
-# placeholder
+'''
+Kalimul Kaif, David Lee, Ethan Saldanha, Mottaqi Abedin
+MKED
+SoftDev 2025
+P01 -- ArRESTed Development
+2025-12-22
+Time spent: 0 hr
+'''
+from flask import Flask
+from flask import request
+from flask import session
+from flask import redirect
+from flask import url_for
+import csv
+import sqlite3
+import random
+import time
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.secret_key = "secret_key_testing"
+DB_FILE = "api_website.db"
